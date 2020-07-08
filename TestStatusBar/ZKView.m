ffd208f561c59029a552d9a74c21d1efb31527e9
zhangkun <840301700@qq.com>
2020-07-08 16:38:21 +0800

    创建新的类

---
+++ TestStatusBar/ZKView.m
@@ -0,0 +1,21 @@
+//
+//  ZKView.m
+//  TestStatusBar
+//
+//  Created by nongbaoling on 2020/7/8.
+//  Copyright © 2020 Frank. All rights reserved.
+//
+
+#import "ZKView.h"
+
+@implementation ZKView
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        
+    }
+    return self;
+}
+
+@end
